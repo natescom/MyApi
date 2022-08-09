24532734cc18184060943e0ca7bb930e2a6ee71d
nathan <nathan16nsb@gmail.com>
2022-08-09 10:31:07 -0300

    api-post

--- pages/api/tempo.js
+++ pages/api/tempo.js
@@ -1,10 +1,19 @@
 function tempo(request, response){
   const dynamicDate = new Date();
 
+  
+  console.log("body", request.body)
+
+  
   response.json({
     date: dynamicDate.toTimeString(),
-    req: request
+    nome: request.body.first,
+    sobrenome: request.body.last
   })
+  
+
+  //response.json(JSON.parse(request.body))
+
 }
 
 export default tempo;
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,16 @@
 function Home(){
-  return <div><h1>Home</h1></div>
+
+  return <div>
+    <h1>Home</h1>
+    <form action="/api/tempo" method="post">
+    <label for="first">Nome:</label>
+    <input type="text" id="first" name="first" /><br></br>
+    <label for="last">Sobrenome: </label>
+    <input type="text" id="last" name="last" /><br></br>
+    <button type="submit">Enviar</button>
+    </form>
+    </div>
 }
+
 
 export default Home;
